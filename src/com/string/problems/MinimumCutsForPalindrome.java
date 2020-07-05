8f35aed62f3db9a8c2eb504314d1a48b02f69b21
Java Samurai <mathurankit@protonmail.com>
2020-07-05 13:00:35 +0530

    Added comments for easy understanding

--- src/com/string/problems/MinimumCutsForPalindrome.java
+++ src/com/string/problems/MinimumCutsForPalindrome.java
@@ -8,9 +8,9 @@ public class MinimumCutsForPalindrome {
     public static void main(String args[]){
         List<String> stringList = Arrays.asList("abdbca", "cddpd", "pqr","pp");
         stringList.stream().forEach((input)->{
-            //List<String> splittedPalindromes = findMinimumCutsForPalindrome(input);
-            int result = minimumCutsForPalindromeRecursively(input, 0, input.length() - 1);
-            System.out.println("Minimum cuts for string : " + input + " : " + result);
+            List<String> splittedPalindromes = findMinimumCutsForPalindrome(input);
+//            int result = minimumCutsForPalindromeRecursively(input, 0, input.length() - 1);
+//            System.out.println("Minimum cuts for string : " + input + " : " + result);
         });
 
     }
@@ -57,6 +57,11 @@ public class MinimumCutsForPalindrome {
         int startIndex = 0;
         int endIndex = startIndex + windowSize;
 
+        /**
+         * In this while block we are building a matrix to know if the substring is a
+         * palindrome or not. This will be used to refer when we are dividing the string
+         *
+         */
         while(windowSize < input.length()){
             while(endIndex < input.length()){
                 String leftCharacter = String.valueOf(input.charAt(startIndex));
@@ -73,27 +78,28 @@ public class MinimumCutsForPalindrome {
             startIndex = 0;
             endIndex = startIndex + windowSize;
         }
+
+        /**
+         * Here we are identifying from start which chunk is maximum palindrome
+         * from start. Here we use the map that is constructed above.
+         */
         int start = 0;
         int end = 0;
         ArrayList<String> palindromes = new ArrayList<>();
         while(end < input.length()){
-            for(int j = start; j < input.length(); j++){
-                if(computeStore[start][j] == 1){
-                    end = j;
+            for(int endSubStringIndex = start; endSubStringIndex < input.length(); endSubStringIndex++){
+                if(computeStore[start][endSubStringIndex] == 1){
+                    end = endSubStringIndex;
                 }
             }
             palindromes.add(input.substring(start, end+1));
             start = end + 1;
             end = start;
         }
+        /**
+         * In the end the total substring that we form are the final answer.
+         * So total cuts = total palindromes formed  - 1
+         */
         return palindromes;
     }
 }
-/**  a b d b c a
- * a 1
- * b   1
- * d     1
- * b       1
- * c         1
- * a           1
- */

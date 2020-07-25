f8f82fed43e89982af563e6bd173518cdf8e5898
Java Samurai <mathurankit@protonmail.com>
2020-07-25 17:29:24 +0530

    Accepted Solution for the Stone Game Leetcode

--- src/com/general/problems/StoneGame.java
+++ src/com/general/problems/StoneGame.java
@@ -1,9 +1,20 @@
 package com.general.problems;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Accepted Solution for Stone Game.
+ */
 public class StoneGame {
     public String stoneGameIII(int[] stoneValue) {
-
-        int aliceMaxScore = performPickup(stoneValue, 0);
+        HashMap<Integer, Integer> cache = new HashMap<>();
+        int aliceMaxScore = performPickup(stoneValue, 0, cache);
+        int[] dpArray = new int[stoneValue.length];
+        for(int index = 0; index < dpArray.length; index++){
+            System.out.print(dpArray[index] + " ");
+        }
+        System.out.println();
         if(aliceMaxScore > 0){
             return "Alice";
         }
@@ -12,23 +23,27 @@ public class StoneGame {
         }
         return "Bob";
     }
-    private int performPickup(int[] stoneValue, int index){
+    private int performPickup(int[] stoneValue, int index, HashMap<Integer, Integer> cache){
         if(index >= stoneValue.length){
             return 0;
         }
-        int playerPick1Stone = stoneValue[index] - performPickup(stoneValue, index + 1);
+        if(cache.containsKey(index)){
+            System.out.println("Look up for Index : " + index);
+            return cache.get(index);
+        }
+        int playerPick1Stone = stoneValue[index] - performPickup(stoneValue, index + 1, cache);
         int playerPick2Stone = Integer.MIN_VALUE;
         int playerPick3Stone = Integer.MIN_VALUE;
         if(index + 1 < stoneValue.length){
-            playerPick2Stone = stoneValue[index] + stoneValue[index + 1] -  performPickup(stoneValue, index + 2);
+            playerPick2Stone = stoneValue[index] + stoneValue[index + 1] -  performPickup(stoneValue, index + 2, cache);
         }
         if(index + 2 < stoneValue.length){
-            playerPick3Stone = stoneValue[index] + stoneValue[index + 1] + stoneValue[index+ 2] - performPickup(stoneValue, index + 3);
+            playerPick3Stone = stoneValue[index] + stoneValue[index + 1] + stoneValue[index+ 2] - performPickup(stoneValue, index + 3, cache);
         }
-        return Math.max(playerPick1Stone, Math.max(playerPick2Stone, playerPick3Stone));
+        int result = Math.max(playerPick1Stone, Math.max(playerPick2Stone, playerPick3Stone));
+        cache.put(index, result);
+        System.out.println("Saving the answer  for Index " + index + " is : " + result );
+        return result;
     }
-
-
-
 
 }
--- src/com/general/problems/StoneGameTest.java
+++ src/com/general/problems/StoneGameTest.java
@@ -40,10 +40,5 @@ public class StoneGameTest {
         assert  result.equalsIgnoreCase("Tie");
     }
 
-    @Test
-    public void test6(){
-        int[] input = {9,-4,0,12,-5,-13,15,6,-16,8,2,16,12,-6,13,0,-16,-11,9,-14,7,-1,14};
-        String result = stoneGame.stoneGameIII(input);
-        assert  result.equalsIgnoreCase("Tie");
-    }
+
 }
